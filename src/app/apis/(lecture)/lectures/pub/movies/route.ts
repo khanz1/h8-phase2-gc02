81ac70dc8349998e0564bb097cb8a1f6618122b9
khanz1 <assistance.xavier@gmail.com>
2024-08-28 08:35:03 +0700

    feat: add pagination filter to lecture pub movies

--- src/app/apis/(lecture)/lectures/pub/movies/route.ts
+++ src/app/apis/(lecture)/lectures/pub/movies/route.ts
@@ -1,13 +1,57 @@
 import prisma from "@/dbs/prisma";
-import { NextResponse } from "next/server";
 import { Lecture_MovieModel } from "@/defs/zod";
+import { validatePublicSearchParams } from "@/defs/zod/x_custom_input";
+import { getQueryOptions, getRequestBody } from "@/utils/data-parser";
+import {
+  getPaginatedResponse,
+  PaginatedApiResponse,
+} from "@/utils/paginated-response";
 import { withErrorHandler } from "@/utils/with-error-handler";
-import { getRequestBody } from "@/utils/data-parser";
+import { Lecture_Movie } from "@prisma/client";
+import { NextResponse } from "next/server";
+// import prisma from "@/dbs/prisma";
+// import { validatePublicSearchParams } from "@/defs/zod/x_custom_input";
+// import { getQueryOptions } from "@/utils/data-parser";
+// import {
+//   getPaginatedResponse,
+//   PaginatedApiResponse,
+// } from "@/utils/paginated-response";
+// import { withErrorHandler } from "@/utils/with-error-handler";
+// import { Movie_Movie } from "@prisma/client";
+// import { NextResponse } from "next/server";
+//
+// export const GET = withErrorHandler(async (_, params) => {
+//   const searchParams = await validatePublicSearchParams(params.searchParams);
+//   const options = getQueryOptions(searchParams, "title", "createdAt", "Genre");
+//
+//   const [movies, rows] = await prisma.$transaction([
+//     prisma.movie_Movie.findMany(options),
+//     prisma.movie_Movie.count({
+//       where: options.where,
+//     }),
+//   ]);
+//
+//   return NextResponse.json<PaginatedApiResponse<Movie_Movie[]>>({
+//     statusCode: 200,
+//     data: getPaginatedResponse(movies, searchParams, rows),
+//   });
+// });
 
-export const GET = withErrorHandler(async () => {
-  const movie = await prisma.lecture_Movie.findMany();
+export const GET = withErrorHandler(async (_, params) => {
+  const searchParams = await validatePublicSearchParams(params.searchParams);
+  const options = getQueryOptions(searchParams, "title", "createdAt", "Genre");
 
-  return NextResponse.json(movie);
+  const [movies, rows] = await prisma.$transaction([
+    prisma.lecture_Movie.findMany(options),
+    prisma.lecture_Movie.count({
+      where: options.where,
+    }),
+  ]);
+
+  return NextResponse.json<PaginatedApiResponse<Lecture_Movie[]>>({
+    statusCode: 200,
+    data: getPaginatedResponse(movies, searchParams, rows),
+  });
 });
 
 export const POST = withErrorHandler(async (req) => {
@@ -19,4 +63,4 @@ export const POST = withErrorHandler(async (req) => {
   });
 
   return NextResponse.json(movie);
-})
+});
