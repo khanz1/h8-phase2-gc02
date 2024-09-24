ebbc344a5b4cccb71c6afd9b398bbced6e790aae
khanz1 <assistance.xavier@gmail.com>
2024-09-24 09:28:08 +0700

    update prisma error

--- src/utils/error-creator.ts
+++ src/utils/error-creator.ts
@@ -1,44 +1,39 @@
 import { HttpError } from "@/utils/http-error";
-import { Prisma } from "@prisma/client";
+import { JWTExpired } from "jose/errors";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
-
-// Dirty hack to check if an object is an instance of a class
-const isObjInstanceOfT = (obj: unknown, T: any) => {
-  try {
-    return obj instanceof T;
-  } catch (e) {
-    return false;
-  }
-};
 
 export const errorCreator = (
   err: unknown,
   code: number = 500,
 ): NextResponse => {
-  console.log(err, "errorCreator");
+  console.log(err, err instanceof JWTExpired, "errorCreator");
   let statusCode = code;
   let errorMessage = "Internal Server Error";
 
   if (err instanceof HttpError) {
     statusCode = err.statusCode;
     errorMessage = err.message;
-
-    // Need to check this first,
-    // since Prisma.PrismaClientKnownRequestError is an instance of Error
-    // Dirty hack to get the error message
-  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+  } else if (err instanceof JWTExpired) {
+    statusCode = 401;
+    errorMessage = "Access token expired, please re-login";
+  } else if (err && typeof err === "object" && "code" in err) {
+    const prismaError = err as { code: string; meta?: any };
     if (
-      err.code === "P2002" &&
-      err.meta &&
-      err.meta.target &&
-      Array.isArray(err.meta.target)
+      prismaError.code === "P2002" &&
+      prismaError.meta &&
+      prismaError.meta.target &&
+      Array.isArray(prismaError.meta.target)
     ) {
       statusCode = 400;
-      errorMessage = `${err.meta.target[0]} - Duplicate key`;
-    } else if (err.code === "P2003" && err.meta && err.meta.field_name) {
+      errorMessage = `${prismaError.meta.target[0]} - Duplicate key`;
+    } else if (
+      prismaError.code === "P2003" &&
+      prismaError.meta &&
+      prismaError.meta.field_name
+    ) {
       statusCode = 400;
-      errorMessage = `${err.meta.field_name} - Foreign key invalid`;
+      errorMessage = `${prismaError.meta.field_name} - Foreign key invalid`;
     }
   } else if (err instanceof ZodError) {
     const errPath = err.errors[0].path[0];
@@ -50,7 +45,6 @@ export const errorCreator = (
     if (
       err.message === "INVALID_TOKEN" ||
       err.name === "JWSSignatureVerificationFailed" ||
-      err.name === "JWTExpired" ||
       err.name === "JWSInvalid" ||
       err.name === "gi" ||
       err.name === "jn" ||
